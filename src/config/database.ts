77f5b53953d8ceb17c13119889fae658d4ef98eb
José Maria de Mattos Netto <mariaze214@gmail.com>
2022-08-31 10:32:15 -0300

    Finalização

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,8 +2,12 @@ import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
 
 export const typeOrmConfig : TypeOrmModuleOptions = {
-  type: 'sqlite',
-  database: 'DB.sqlite',
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'pguser',
+  password: 'pgpassword',
+  database: 'nestjs',
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
 };
